eacca3435de4e337c42d6d976f109c30fd14575c
Martins Siksnis <msiksnis@gmail.com>
2023-11-12 11:59:27 +0100

    improved hamburger menu

--- components/js/load-header.js
+++ components/js/load-header.js
@@ -3,6 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((response) => response.text())
     .then((data) => {
       document.querySelector("body").insertAdjacentHTML("afterbegin", data);
+
+      // Now that the header is loaded, find the elements and add event listeners
+      const hamburgerMenu = document.querySelector(".hamburger-menu");
+      const navUl = document.querySelector("nav .nav-links");
+
+      hamburgerMenu.addEventListener("click", () => {
+        navUl.classList.toggle("show");
+        hamburgerMenu.classList.toggle("open");
+      });
     })
     .catch((error) => console.error("Error loading the header:", error));
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
