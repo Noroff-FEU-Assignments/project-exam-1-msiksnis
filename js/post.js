bb7edd467447b9d27577f5721dc32daeeb4886ab
Martins Siksnis <msiksnis@gmail.com>
2023-11-21 22:38:11 +0100

    working on single post page

--- js/post.js
+++ js/post.js
@@ -2,6 +2,7 @@ import { fetchPostBySlug } from "./api.js";
 
 const queryParams = new URLSearchParams(window.location.search);
 const postSlug = queryParams.get("slug");
+const title = document.querySelector("title");
 
 document.addEventListener("DOMContentLoaded", () => {
   if (postSlug) {
@@ -11,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
           displayPost(post);
         } else {
           console.log("No post found for this slug");
-          // Handle the case where no post is found
+          // Add error handling for the case when no post is found
         }
       })
       .catch((error) => console.error("Error fetching post:", error));
@@ -19,16 +20,33 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function displayPost(post) {
-  const postContent = document.getElementById("post-content");
+  title.textContent = post.title.rendered;
+  // Populate the title
+  document.querySelector(".post-title").innerText = post.title.rendered;
 
-  const postTitle = document.createElement("h1");
-  postTitle.innerText = post.title.rendered;
-  postContent.appendChild(postTitle);
+  // Populate the content
+  document.getElementById("post-body").innerHTML = post.content.rendered;
 
-  // ... Create and append other post elements like content, date, image ...
-
-  // If you want to display the featured image
+  // Fetch and display the featured image
   if (post.featured_media) {
-    // Fetch and display the image as in the fetchPosts function
+    fetchFeaturedMedia(post.featured_media).then((mediaUrl) => {
+      if (mediaUrl) {
+        document.getElementById("post-image").src = mediaUrl;
+        document.getElementById("post-image").alt = post.title.rendered;
+      }
+    });
+  }
+}
+
+async function fetchFeaturedMedia(mediaId) {
+  try {
+    const response = await fetch(
+      `https://www.lovetherain.no/wp-json/wp/v2/media/${mediaId}`
+    );
+    const media = await response.json();
+    return media.source_url;
+  } catch (error) {
+    console.error("Error fetching featured media:", error);
+    return null;
   }
 }
