31750e44638b7792795c10117149ecfc2174ff16
Martins Siksnis <msiksnis@gmail.com>
2023-12-02 20:45:57 +0100

    working on search modal

--- components/js/load-header.js
+++ components/js/load-header.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((data) => {
       document.querySelector("body").insertAdjacentHTML("afterbegin", data);
 
+      // Code related to hamburger menu
       const hamburgerContainer = document.querySelector(".hamburger-container");
       const hamburgerMenu = document.querySelector(".hamburger-menu");
       const navUl = document.querySelector("nav .nav-links");
@@ -13,19 +14,62 @@ document.addEventListener("DOMContentLoaded", () => {
         hamburgerMenu.classList.toggle("open");
       });
 
-      // Add the active class to the current page link
-      const currentPath = window.location.pathname;
-      const navLinks = document.querySelectorAll("nav .nav-link a");
+      // Code related to search modal
+      const searchButton = document.querySelector(".search-btn");
+      const closeButton = document.querySelector(".close-btn");
+      const modal = document.getElementById("search-modal");
+      // const modalContent = document.querySelector(".modal-content");
 
-      navLinks.forEach((link) => {
-        if (link.getAttribute("href") === currentPath) {
-          link.classList.add("active");
+      if (searchButton) {
+        searchButton.addEventListener("click", function () {
+          modal.classList.add("active");
+        });
+      }
+
+      if (closeButton) {
+        closeButton.addEventListener("click", function () {
+          modal.classList.remove("active");
+        });
+      }
+
+      // Close modal when clicking outside of modal-content
+      modal.addEventListener("click", function (event) {
+        if (event.target === modal) {
+          modal.classList.remove("active");
         }
       });
+
+      function debounce(func, wait, immediate) {
+        let timeout;
+        return function () {
+          const context = this,
+            args = arguments;
+          const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+          };
+          const callNow = immediate && !timeout;
+          clearTimeout(timeout);
+          timeout = setTimeout(later, wait);
+          if (callNow) func.apply(context, args);
+        };
+      }
+
+      const debouncedSearch = debounce(function () {
+        const searchValue = document.getElementById("search-input").value;
+        // Implement search functionality here
+        console.log("Searching for:", searchValue);
+      }, 250);
+
+      const searchInput = document.getElementById("search-input");
+      if (searchInput) {
+        searchInput.addEventListener("input", debouncedSearch);
+      }
     })
     .catch((error) => console.error("Error loading the header:", error));
 });
 
+// Scroll event listener
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   if (window.scrollY > 50) {
